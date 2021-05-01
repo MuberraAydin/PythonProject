53ec6a6dfbe78b77e33f0f92ddf647c725ce0748
Müberra Aydın <muberra.aydin.008@gmail.com>
2021-05-01 18:20:56 +0300

    create saat.py

---
+++ src/saat.py
@@ -0,0 +1,23 @@
+from tkinter import Label, Tk
+import time
+
+app_window = Tk()
+app_window.title("My Digital Time")
+app_window.geometry("650x450")
+app_window.resizable(30,30)
+
+text_font= ("Boulder", 48, 'bold')
+background = "Black"
+foreground = "White"
+border_width = 200
+
+label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width)
+label.grid(row=0, column=0)
+
+def digital_clock():
+    time_live = time.strftime("%H:%M:%S")
+    label.config(text=time_live)
+    label.after(100, digital_clock)
+
+digital_clock()
+app_window.mainloop()
